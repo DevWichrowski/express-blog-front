fe47ea60520b58bf05507384debe948b0536c82a
DevWichrowski <wichrowski.patryk92@gmail.com>
2019-11-23 17:02:56 +0100

    Add posts actions

---
+++ src/store/actions/posts.actions.js
@@ -0,0 +1,18 @@
+export const GET_POSTS_PENDING = 'GET_POSTS';
+export const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS';
+export const GET_POSTS_FAILURE = 'GET_POSTS_FAILURE';
+
+
+export const getPostsPending = () => ({
+    type: GET_POSTS_PENDING,
+});
+
+export const getPostsSuccess = payload => ({
+    type: GET_POSTS_SUCCESS,
+    payload
+});
+
+export const getPostsFailure = payload => ({
+    type: GET_POSTS_FAILURE,
+    payload
+});
