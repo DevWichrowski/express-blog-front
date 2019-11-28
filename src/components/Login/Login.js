8a8c3b58e8863222c9ca2a315ce687ca617f9071
Patryk Wichrowski <wichrowski.patryk92@gmail.com>
2019-11-28 14:31:22 +0100

    Save login and password

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "./Login.scss";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
 const Login = props => {
+
+    const [login, setLogin] = useState(null);
+    const [password, setPassword] = useState(null);
+
+    const submit = () => {
+        console.log('login', login);
+        console.log('password', password);
+    };
+
     return (
         <div className="login">
             <h1>Login</h1>
-            <form action="POST" className="login-form">
-                <TextField id="outlined-basic" label="Login" variant="outlined" className="form-text-field"/>
-                <TextField id="outlined-basic" label="Password" variant="outlined" type="password"
-                           className="form-text-field"/>
-                <Button variant="contained" color="primary">
+            <form className="login-form">
+                <TextField
+                    className="form-text-field"
+                    id="outlined-basic"
+                    label="Login"
+                    variant="outlined"
+                    onChange={e => setLogin(e.target.value)}
+                />
+                <TextField
+                    className="form-text-field"
+                    id="outlined-basic"
+                    label="Password"
+                    variant="outlined"
+                    type="password"
+                    onChange={e => setPassword(e.target.value)}
+                />
+                <Button variant="contained" color="primary" onClick={submit}>
                     Login
                 </Button>
             </form>
