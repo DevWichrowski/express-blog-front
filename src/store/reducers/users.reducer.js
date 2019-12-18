c066723232acc0d4af0883341293de135462ecc7
DevWichrowski <wichrowski.patryk92@gmail.com>
2019-12-18 20:55:47 +0100

    Add selectors for users

--- src/store/reducers/users.reducer.js
+++ src/store/reducers/users.reducer.js
@@ -5,6 +5,7 @@ import * as UsersActions from "../actions/users.actions";
 const initialState = {
     pending: false,
     user: null,
+    users: [],
 };
 
 export const usersReducer = (state = initialState, action) =>
@@ -20,6 +21,19 @@ export const usersReducer = (state = initialState, action) =>
                 break;
             }
             case UsersActions.GET_MY_PROFILE_FAILURE: {
+                draft.pending = false;
+                break;
+            }
+            case UsersActions.GET_USERS_PENDING: {
+                draft.pending = true;
+                break;
+            }
+            case UsersActions.GET_USERS_SUCCESS: {
+                draft.users = [...action.payload];
+                draft.pending = true;
+                break;
+            }
+            case UsersActions.GET_USERS_FAILURE: {
                 draft.pending = false;
                 break;
             }
