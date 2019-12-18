89fa566d49a0dbbe8bf6071b43b49bb1869e08cf
DevWichrowski <wichrowski.patryk92@gmail.com>
2019-12-18 20:28:13 +0100

    Add actions for users

---
+++ src/components/MenageUsers/MenageUsers.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import "./MenageUsers.scss";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+
+
+const MenageUsers = props => {
+    return (
+        <div className="administrate-users">
+            <Paper className="user-paper">
+                <Table className="user-table" aria-label="simple table">
+                    <TableHead className="table-head">
+                        <TableRow>
+                            <TableCell className="user-head-cell">Avatar</TableCell>
+                            <TableCell className="user-head-cell">Nickname</TableCell>
+                            <TableCell className="user-head-cell">Login</TableCell>
+                            <TableCell className="user-head-cell">Password</TableCell>
+                            <TableCell className="user-head-cell">E-mail</TableCell>
+                            <TableCell className="user-head-cell" align="right">Actions</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {/*{props.allPosts.map(row => (*/}
+                        <TableRow>
+                            <TableCell component="th" scope="row">
+                                (-,-)
+                            </TableCell>
+                            <TableCell component="th" scope="row">
+                                Test
+                            </TableCell>
+                            <TableCell>asd</TableCell>
+                            <TableCell>asd</TableCell>
+                            <TableCell>asd</TableCell>
+                            <TableCell align="right">
+                                <DeleteIcon className="delete-icon"/>
+                                {/*<NavLink to={`/edit-post/${row._id}`}>*/}
+                                <EditIcon/>
+                                {/*</NavLink>*/}
+                                {/*<NavLink to={`/single-post/${row._id}`}>*/}
+                                <VisibilityIcon/>
+                                {/*</NavLink>*/}
+                            </TableCell>
+                        </TableRow>
+
+                        <TableRow>
+                            <TableCell component="th" scope="row">
+                                (-,-)
+                            </TableCell>
+                            <TableCell component="th" scope="row">
+                                Test
+                            </TableCell>
+                            <TableCell>asd</TableCell>
+                            <TableCell>asd</TableCell>
+                            <TableCell>asd</TableCell>
+                            <TableCell align="right">
+                                <DeleteIcon className="action-icon"/>
+                                {/*<NavLink to={`/edit-post/${row._id}`}>*/}
+                                <EditIcon className="action-icon"/>
+                                {/*</NavLink>*/}
+                                {/*<NavLink to={`/single-post/${row._id}`}>*/}
+                                <VisibilityIcon className="action-icon"/>
+                                {/*</NavLink>*/}
+                            </TableCell>
+                        </TableRow>
+                        {/*))}*/}
+                    </TableBody>
+                </Table>
+            </Paper>
+        </div>
+    );
+};
+
+export default MenageUsers;
--- src/store/actions/users.actions.js
+++ src/store/actions/users.actions.js
@@ -2,6 +2,10 @@ export const GET_MY_PROFILE_PENDING = 'GET_MY_PROFILE_PENDING';
 export const GET_MY_PROFILE_SUCCESS = 'GET_MY_PROFILE_SUCCESS';
 export const GET_MY_PROFILE_FAILURE = 'GET_MY_PROFILE_FAILURE';
 
+export const GET_USERS_PENDING = 'GET_USERS_PENDING';
+export const GET_USERS_SUCCESS = 'GET_USERS_SUCCESS';
+export const GET_USERS_FAILURE = 'GET_USERS_FAILURE';
+
 export const CLEAR_USER = "CLEAR_USER";
 
 export const getMyProfilePending = payload => ({
@@ -16,6 +20,22 @@ export const getMyProfileSuccess = payload => ({
 
 export const getMyProfileFailure = payload => ({
     type: GET_MY_PROFILE_FAILURE,
+    payload
+});
+
+
+export const getUsersPending = payload => ({
+    type: GET_USERS_PENDING,
+    payload
+});
+
+export const getUsersSuccess = payload => ({
+    type: GET_USERS_SUCCESS,
+    payload
+});
+
+export const getUsersFailure = payload => ({
+    type: GET_USERS_FAILURE,
     payload
 });
 
